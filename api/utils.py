92573586afdf279212b2dbc182231976354deb01
timtonix <timtonic@outlook.fr>
2023-09-20 17:25:32 +0200

    update regex

--- api/utils.py
+++ api/utils.py
@@ -4,13 +4,12 @@ import api.bdd as bdd
 
 def handle_connection(data: str, addr: tuple):
     print("HELLLLOOO")
-    m = re.match(r"p(i | o)ng (\w+) (\d+)", data)
+    m = re.match(r"p[i|o]ng (\w+) (\d+)", data)
     if m:
-        print(m.group())
+        print(m.groups())
         pseudo = m.group(1)
         port = int(m.group(2))
         ip = addr[0]
-        print(ip)
         if len(pseudo) < 20 and 50000 < port < 65635:
             bdd.add_peer(pseudo, ip, port)
             return port
